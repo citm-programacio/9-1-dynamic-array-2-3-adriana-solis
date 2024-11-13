f511fd320b887774cec5b20199d52fff009399d9
adriana-solis <adriana.solis@estudiantat.upc.edu>
2024-11-13 13:13:41 +0100

    Update dynamic_array.cpp

--- dynamic_array.cpp
+++ dynamic_array.cpp
@@ -1,6 +1,3 @@
-// dynamic_array.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 using namespace std;
 
@@ -29,7 +26,7 @@ int* order(int* oldArray, int len, int n)
         }
     }
     
-
+    delete[] oldArray;
     return newArray;
 }
 
@@ -60,6 +57,7 @@ int main()
         }
         else
         {
+            cout << "Thanks for playing!";
             break;
         }
 
@@ -67,13 +65,3 @@ int main()
     } while (num != -1);
 
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
